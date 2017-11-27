1eda4df3b3b137f7c72bb2640ecd54cb48346a10
Jawann <jawann@gmail.com>
2017-11-27 02:42:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line (array)
+  return "The line is currently empty." if array.nil?
+  temp = "The line is currently:"
+  array.each_with_index {|x, i| temp << " #{i+1}. #{x}"}
+  temp
+end
+
+def take_a_number (array, name)
+  array << name
+  "Welcome, #{name}.  You are number #{array.count} in line."
+end
+
+def now_serving (array)
+  array.nil? ? "There is nobody waiting to be served!" : "Currently serving #{array.shift}"
+end
